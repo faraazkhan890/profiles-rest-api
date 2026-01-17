d7a8f3c07401776b4e408e1f5e20f90951605ef8
ABC <ABC@gmail.com>
2026-01-17 12:25:12 +0530

    Enable Django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -5,14 +5,15 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserProfileManager(BaseUserManager):
-    """manager for user profiles"""
+    """Manager for user profiles"""
 
     def create_user(self, email, name, password=None):
-        """Create new user profile"""
+        """Create a new user profile"""
         if not email:
-            raise ValueError('user must have an email address')
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
+        # Pass name here so it is saved to the model
         user = self.model(email=email, name=name)
 
         user.set_password(password)
@@ -20,9 +21,10 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-        def create_suoeruser(self, email, name, password):
-            """create and save a new superuser with given details"""
-            user = self.create_user(email, name, password)
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        # Pass email and name to the create_user method above
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
@@ -30,30 +32,31 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
+
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
-    email = models.EmailField(max_length=255, unique = True)
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff =models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIREd_FIELDS = ['name']
+    # Since 'name' is here, create_superuser MUST accept it as an argument
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrive full name of user"""
+        """Retrieve full name of user"""
         return self.name
 
     def get_short_name(self):
-        """Retrive short name of user"""
+        """Retrieve short name of user"""
         return self.name
 
-    def __str__(self):
+    def _str_(self):
         """Return string representation of our user"""
-        return self.name
-
+        return self.email
 
 
 
